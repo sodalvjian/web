7f699715d708f6c5490ad1c8fbe0433735bdafee
xsstc1 <xsstc1@163.com>
2021-09-15 21:10:58 +0800

    feat: 修改首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,40 +6,38 @@
           class="home-logo mt-20"
           src="../../assets/img/melax_logo.png"
           alt=""
-      /></a>
+        /></a>
     </header>
     <section class="mercury-banner">
       <el-row class="container">
-        <el-col :sm="10" class="color-white">
+        <el-col :sm="12" class="color-white">
           <h1><img src="../../assets/img/logo_mercury.png" alt="" /></h1>
-          <div class="mt-30 f18 lh2">
+          <div class="mt-30 f17 lh1-5">
             Mercury Natural Language Processing makes quick work of your medical
-            data extraction projects-- accurately and at a fraction of the cost
+            data extraction projects — accurately and at a fraction of the cost
             of other web-based products.
           </div>
           <div class="mt-50">
             <router-link to="/realtimeAnalysis">
               <el-button
                 type="danger"
+                style="height:45px"
                 class="f16 mercury-try-it"
                 :style="'background:#4ebebf;border:1px solid #4ebebf'"
               >
                 Try it now
-              </el-button></router-link
-            >
+              </el-button></router-link>
           </div>
         </el-col>
       </el-row>
     </section>
     <section class="container">
       <el-row class="mercury-nav " :gutter="20">
-        <el-col :sm="6" class="mt-20">
-          <el-card :body-style="{ padding: '30px' }" shadow="always">
+        <el-col :sm="6" class="mt-20" align="center">
+          <el-card :body-style="{ padding: '15px' }" shadow="always">
             <div>
-              <svg class="icon f22" aria-hidden="true">
-                <use xlink:href="#icon-price" />
-              </svg>
-              <span class="ml-10 fb f20">Affordable</span>
+              <img src="../../assets/img/home/nav_one.png" alt="" />
+              <div class="ml-10 f19">Affordable Price</div>
             </div>
             <div
               class="mt-15 f15 lh2 mercury-nav-content"
@@ -50,30 +48,26 @@
             </div>
           </el-card>
         </el-col>
-        <el-col :sm="6" class="mt-20">
-          <el-card :body-style="{ padding: '30px' }" shadow="always">
+        <el-col :sm="6" class="mt-20" align="center">
+          <el-card :body-style="{ padding: '15px' }" shadow="always">
             <div>
-              <svg class="icon f22" aria-hidden="true">
-                <use xlink:href="#icon-pipelines" />
-              </svg>
-              <span class="ml-10 fb f20">More Pipelines</span>
+              <img src="../../assets/img/home/nav_two.png" alt="" />
+              <div class="ml-10 f19">Flexible Pipelines</div>
             </div>
             <div
               class="mt-15 f15 lh2 mercury-nav-content"
               title="More pipelines - with six medical pipelines and more added regularly extract only the data you need"
             >
-              Six medical pipelines and more added regularly to extract only the
-              data you need
+              Five medical pipelines and more added regularly to extract only
+              the data you need
             </div>
           </el-card>
         </el-col>
-        <el-col :sm="6" class="mt-20">
-          <el-card :body-style="{ padding: '30px' }" shadow="always">
+        <el-col :sm="6" class="mt-20" align="center">
+          <el-card :body-style="{ padding: '15px' }" shadow="always">
             <div>
-              <svg class="icon f22" aria-hidden="true">
-                <use xlink:href="#icon-Analysis" />
-              </svg>
-              <span class="ml-10 fb f20">Analysis</span>
+              <img src="../../assets/img/home/nav_three.png" alt="" />
+              <div class="ml-10 f19">High Performance</div>
             </div>
             <div
               class="mt-15 f15 lh2 mercury-nav-content"
@@ -83,13 +77,11 @@
             </div>
           </el-card>
         </el-col>
-        <el-col :sm="6" class="mt-20">
-          <el-card :body-style="{ padding: '30px' }" shadow="always">
+        <el-col :sm="6" class="mt-20" align="center">
+          <el-card :body-style="{ padding: '15px' }" shadow="always">
             <div>
-              <svg class="icon f22" aria-hidden="true">
-                <use xlink:href="#icon-Deploy" />
-              </svg>
-              <span class="ml-10 fb f20">Security</span>
+              <img src="../../assets/img/home/nav_four.png" alt="" />
+              <div class="ml-10 f19">On Premise Installation</div>
             </div>
             <div
               class="mt-15 f15 lh2 mercury-nav-content"
@@ -162,7 +154,7 @@
               <h2>AFFORDABLE</h2>
                <div class="mt-15 f15 lh2 mercury-nav-content">
                 Give me some copywriting
-              </div> 
+              </div>
             </el-card>
           </el-col>
         </el-row>
@@ -175,13 +167,31 @@
         shadow="always"
         class="bordernone bc w85"
       >
-        <main class="mt-15 color-6 f15">
+        <main class="mt-15 f15">
           <div>
-            MERCURY NLP is user-friendly: cut & paste data, apply a pipeline and
-            filters, get real-time analytical results. Spend only as much as you
-            need to process a job. Mercury NLP is pay-per-byte and half the
-            price of our competitors. Your jobs are your business. Select
-            privacy options and know your NLP job is secure and HIPAA compliant.
+            <p>
+              MERCURY NLP is Melax Tech’s breakthrough software for Natural
+              Language Processing in healthcare and related industries. Mercury
+              NLP offers users the ability to extract textual data in a simple
+              to use DIY format, backed by our expert data and development
+              support team. Mercury NLP’s pay-per-byte system allows users to
+              automate and speed up data extraction with unparalleled accuracy
+              and flexibility.
+            </p>
+            <p>
+              At half of the price of competitors, Mercury NLP allows
+              administrators to continually monitor and refine projects. The
+              results are processes and outputs that can be reproduced by
+              project teammates for better, more reliable analytics reporting.
+            </p>
+
+            <p>
+              With five pre-built clinical pipelines and more on the way, users
+              are able to apply a pipeline to text data, process it and see
+              results in record time. Mercury NLP is HIPAA compliant; client
+              data is never divulged to or used by anyone other than client team
+              members.
+            </p>
           </div>
           <!-- <div>
             <img
@@ -194,7 +204,40 @@
       </el-card>
     </section>
     <footer class="mercury-footer tc">
-      © 2021 MELAX℠ All Rights Reserved.
+      <div class="container">
+        <el-row>
+          <el-col :span="4">
+            <a
+              href="https://twitter.com/MelaxTech?s=20"
+              class="mr-10"
+              target="_blank"
+            >
+              <img
+                width="20"
+                height="20"
+                src="https://melaxtech.com/wp-content/themes/melax-wordpress/assets/img/icon/twitter.webp"
+                alt="/"
+              /></a>
+            <a
+              href="https://www.linkedin.com/company/melaxtech/"
+              target="_blank"
+            >
+              <img
+                width="20"
+                height="20"
+                src="https://melaxtech.com/wp-content/themes/melax-wordpress/assets/img/icon/linkedin.webp"
+                alt="/"
+              /></a>
+          </el-col>
+          <el-col
+            :span="16"
+            class="pt-5 footer-copy-right-text color-white tc f13"
+          >
+            © 2021 MELAX℠ All Rights Reserved.
+          </el-col>
+          <el-col :span="4"></el-col>
+        </el-row>
+      </div>
     </footer>
   </div>
 </template>
@@ -220,6 +263,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  max-width: 1140px;
+}
 .mercury-home {
   font-family: Arial, Helvetica, sans-serif;
 }
@@ -229,10 +275,10 @@ export default {
   }
 }
 .home-logo {
-  width: 280px;
+  width: 230px;
 }
 .mercury-banner {
-  height: 720px;
+  height: 670px;
   margin-top: -100px;
   padding-top: 200px;
   background-image: url('../../assets/img/mercury_banner.png');
@@ -240,7 +286,7 @@ export default {
   background-repeat: no-repeat;
   background-position: center center;
   img {
-    width: 250px;
+    width: 260px;
   }
 }
 .mercury-try-it {
@@ -250,8 +296,12 @@ export default {
 }
 .mercury-nav {
   margin-top: -100px;
+  img {
+    width: 40px;
+  }
   .mercury-nav-content {
     min-height: 90px;
+    color: #555;
     text-overflow: -o-ellipsis-lastline;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -274,7 +324,8 @@ export default {
 .mercury-color-description {
   margin-top: -100px;
   main {
-    line-height: 3;
+    line-height: 1.7;
+    color: #555555;
   }
 }
 .mercury-footer {
